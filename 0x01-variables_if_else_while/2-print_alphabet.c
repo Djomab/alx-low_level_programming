6976b46d6b418c3caf643a5f42109cb6bf27ad3c
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-12 05:50:08 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'a'; ++c)
+	printf("%c/n", c);
+	return (0);
+}
