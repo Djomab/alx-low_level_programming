a6fb83210f6ad91454ee36730e9509ad012058d6
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-16 14:55:35 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,18 @@
+#ifndef MAIN_H
 #define MAIN_H
 
 /* _putchar.c */
 int _putchar(char);
+void print_alphabet(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
+
+#endif
