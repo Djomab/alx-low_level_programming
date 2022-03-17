e49fc51bdc09e7c93793047ebcef4ed1a144f316
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-17 14:44:57 -0700

    void print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_diagonal - prints the numbers, from 0 to 9 except 2 and 4
+ *Description : some descr
+ * @n: number
+ * Return: void
+ *
+ */
+void print_diagonal(int n)
+{
+	int i = 0, k = 0;
+
+	if (n > 0)
+	{
+		while (k < n)
+		{
+			while (i < k)
+			{
+				_putchar(' ');
+				i++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i = 0;
+			k++;
+		}
+	}
+	else
+		_putchar('\n');
+}
