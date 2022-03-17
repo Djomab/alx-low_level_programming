6f207582fb156679bf4a2627620e228285aedbf3
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-17 13:22:26 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: void
+ *
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c >= '0' && c <= '9')
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
