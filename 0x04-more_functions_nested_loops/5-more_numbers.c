deb2db532540af54fc6c2310f36aa2104a4e9c62
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-17 13:43:53 -0700

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *more_numbers -  prints 10 times the numbers, from 0 to 14
+ *
+ * Return: void
+ *
+ */
+void more_numbers(void)
+{
+	int c;
+	int i = 0;
+
+	while (i < 10)
+		for (c = '0'; c <= '14'; c++)
+		{
+			_putchar(c);
+		}
+		i++;
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -19,4 +19,7 @@ void print_numbers(void);
 /* */
 void print_most_numbers(void);
 
+/* */
+void more_numbers(void);
+
 #endif
