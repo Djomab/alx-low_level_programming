4a1c9fa9168a2ce8582aec261bf5c58de0566318
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-18 14:50:59 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98- checks for alphabetic character.
+ *
+ * @n : number
+ * Return: always 0
+ */
+int print_to_98(int n)
+{
+	int i;
+
+	if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			printf('%d', n);
+			n--;
+		}
+	}
+	else
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf('%d', i);
+		}
+	}
+}
