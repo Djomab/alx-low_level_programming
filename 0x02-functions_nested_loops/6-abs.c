1ca8d0ba6819b7ded1ef70b1d32ea0bf04ca9722
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-18 06:03:47 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs- checks for alphabetic character.
+ *
+ * @n : character
+ * Return: always 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,4 +16,6 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
+int _abs(int);
+
 #endif
