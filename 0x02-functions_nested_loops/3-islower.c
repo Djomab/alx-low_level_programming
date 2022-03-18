d44b54fad15eafc5465d7263c685dd757b59687a
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-18 03:56:07 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower- checks for lowercase character
+ *
+ * @c : character
+ * Return: always 0
+ */
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+
+	return(0);
+}
