6de2861ae6268ca6646d79f07ae1ce2012698cca
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-18 15:31:16 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_square - prints the numbers, from 0 to 9 except 2 and 4
+ * @size: number
+ * Return: void
+ *
+ */
+void print_square(int size)
+{
+	int i = 0, k = 0;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (k = 0; k < size; k++)
+			{
+				_putchar(k);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -27,4 +27,6 @@ void print_line(int n);
 
 void print_diagonal(int n);
 
+void print_square(int size);
+
 #endif
