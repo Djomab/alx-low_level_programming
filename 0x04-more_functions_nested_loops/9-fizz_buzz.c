2959fafd4925b291fa0dea7fd9abee5bea97cd59
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-18 15:58:47 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * fizzbuzz - prints the numbers, from 0 to 9 except 2 and 4
+  * Return: void
+ *
+ */
+void fizzbuzz(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if (i % 3 == 0)
+		{
+
+			printf("Fizz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -29,4 +29,6 @@ void print_diagonal(int n);
 
 void print_square(int size);
 
+void fizzbuzz(void);
+
 #endif
