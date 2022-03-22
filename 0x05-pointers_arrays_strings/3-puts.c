19d5e5e5f3c0f0db9137e69fd254d36b786a43c4
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-03-22 06:56:39 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts- updates the value it points to to 98.
+ * @s : first param
+ * Return: lenght of s.
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
