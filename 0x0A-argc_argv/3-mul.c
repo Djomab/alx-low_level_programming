f0ad7bc481d3a7d782c12c76eb2b8d4b0edeedb5
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-04-04 12:16:41 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name
+ * @argc: arguments count
+ * @argv: arguments value
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int multiplication;
+
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	multiplication = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", multiplication);
+
+	return (0);
+}
