606c4cf53339f7a04f303513859fae381d83d1ca
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-04-05 08:43:36 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *tab;
+	unsigned int i;
 
 	tab = malloc(sizeof(c) * size);
 
 	if (size == 0)
 		return (NULL);
+
+	if (tab != NULL)
+	{
+		for (i = 0; i < size; i++)
+		tab[i] = c;
+	}
+
 	return (tab);
 
 }
