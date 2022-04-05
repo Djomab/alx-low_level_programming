be282c6f443f105419bbd76295b265db4eeaa450
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-04-05 11:34:57 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *  str_concat - creates an array of chars, and initializes it with char
+ * @s1: character
+ * @s2: character
+ * Return: array or null
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	int i = 0, len1 = 0, len2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (s3 != NULL)
+	{
+		while (s2[i] != '\0')
+		{
+			s1[len1 + i + 1] = s2[i];
+			s3[i] = s1[len1 + i + 1];
+			i++;
+		}
+		s3[i] = '\0';
+	}
+
+	return (s1);
+
+}
