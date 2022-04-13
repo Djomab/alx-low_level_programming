e910f152fba984a88aec08f15a97c006cdc38680
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-04-12 19:06:48 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure
+ * @name: name of dog
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
