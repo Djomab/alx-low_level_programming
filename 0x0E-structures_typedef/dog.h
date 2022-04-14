43e0644f3dbbeac08b26a6ed5c0e6b88d4a046e7
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-04-13 19:16:22 -0700

    modifs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,8 +18,13 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-/*dog_t *new_dog(char *name, float age, char *owner);
+/**
+ * dog_t - typedef for structure
+ */
+typedef struct dog dog_t;
 
-void free_dog(dog_t *d);*/
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
