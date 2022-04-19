5073f7beabadde735e335a7e68be0ff4aa7dbfe1
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-04-19 06:57:43 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,9 +17,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
-	{	
+	{
 		printf("%d", va_arg(args, int));
-	
 		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 
