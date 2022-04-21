b4c50bbacfac11fbcf52f18a7abc570124c46ca8
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-04-21 11:29:23 -0700

    rep init

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - prints all the elements of a list_t
+ * @h: pointeur de type list_t
+ * Return: void
+ */
+
+size_t print_list(const list_t *h)
+{
+	
+}
