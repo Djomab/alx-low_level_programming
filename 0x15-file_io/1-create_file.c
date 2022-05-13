1c496c8ffc634aed6994bac33b53a5efbae1111b
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-05-13 07:31:03 -0700

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: content
+ * Return: 0
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0; /*file descriptor*/
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		write(fd, text_content, strlen(text_content));
+	}
+
+	close(fd);
+
+	return (1);
+
+}
