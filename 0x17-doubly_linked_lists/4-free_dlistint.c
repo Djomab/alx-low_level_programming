20ff03ce55358d8fdb620fc797c640c18df122ab
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-16 10:27:21 -0700

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - free all allocated memory.
+ * @head: head of linked list.
+ *
+ * Return: void function.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
