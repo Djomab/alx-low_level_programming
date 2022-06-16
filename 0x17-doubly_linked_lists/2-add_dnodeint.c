fd5591166c62a4ed2cbd8bc31e58c7efd45289bd
Djogona Mahamat Belna <djogonamahamatbelna@gmail.com>
2022-06-16 07:37:12 -0700

    2. Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,10 +15,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (new == NULL)
 		return (NULL);
+	
 	new->n = n;
+	new->next = NULL;
 	new->prev = NULL;
-	new->next = *head;
-	*head = new;
+
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
 
 	return (new);
 }
