ddd986a8876a1f25343da7309ac0c9c3d51ca402
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-08-29 21:41:20 +0100

    100-atoi.c compliant to betty

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,13 +13,14 @@ int _atoi(char *s)
 	int i = 0, sign = 1;
 
     /*saut des espaces blancs et caractère alphabétique*/
-    while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '+' || s[i] == '-' || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-    {
-        if (s[i] == '-') {
-            sign = -1 * sign;
-        }
-        i++;
-    }
+	while (!('0' <= s[i] && s[i] <= '9') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign = -1 * sign;
+		}
+		i++;
+	}
 
 	while (s[i] != '\0' && '0' <= s[i] && s[i] <= '9')
 	{
