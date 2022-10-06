a5c6ea6801ce21dda4ce336252f040d969a50b23
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-10-06 17:58:38 +0100

    0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+
+/**
+ * linear_search - linear search algo
+ * @array: array in which to search
+ * @size: size of array
+ * @value: searched value
+ * Return: value or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = %d\n\n", i, array[i]);
+		if (value == array[i])
+		{
+			return (value);
+		}
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,4 @@
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
+int linear_search(int *array, size_t size, int value);
+#endif
